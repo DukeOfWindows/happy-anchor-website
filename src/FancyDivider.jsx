dcf989ad230127c85b08b21b4101f0a58242f015
Hogan Gill <hoganrgill@gmail.com>
2025-06-14 08:22:20 +1200

    Fancy divider changed to logo

--- src/FancyDivider.jsx
+++ src/FancyDivider.jsx
@@ -4,7 +4,7 @@ export default function FancyDivider({ bgColor = "bg-white" }) {
   return (
 <div className="my-12 flex items-center justify-center space-x-4">
   <div className="w-24 h-px bg-red-700"></div>
-  <span className="text-red-700 text-xl">⚓</span>
+<img  src="/images/logo-small.png" alt="Logo" className="h-6 w-auto"/>
   <div className="w-24 h-px bg-red-700"></div>
 </div>
   );
